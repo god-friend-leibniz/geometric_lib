9a102f3a2eaceb2196143ba7ff8595ff31ce0d52
Evarist Chubarov <god.friend.leibniz@gmail.com>
2025-12-04 17:19:37 +0300

    added unit tests

--- square.py
+++ square.py
@@ -1,14 +1,43 @@
-import math
+import unittest
 
-def area(side: float) -> float:
+def area(side: float) -> float | bool:
     '''
     Given the side of a square, calculates area
     '''
-    return math.pow(side, 2)
+    if side < 0:
+        return False
+    return side * side
 
 
 def perimeter(side: float) -> float:
     '''
     Given the side of a square, calculates perimeter
     '''
+    if side <= 0:
+        return False
     return 4 * side
+
+class SquareTestCase(unittest.TestCase):
+    def test_area_zero_mul(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_area_pos_mul(self):
+        res = area(2)
+        self.assertEqual(res, 4)
+
+    def test_area_neg_mul(self):
+        res = area(-3)
+        self.assertFalse(res)
+
+    def test_perimeter_zero_add(self):
+        res = perimeter(0)
+        self.assertFalse(res)
+
+    def test_perimeter_pos_add(self):
+        res = perimeter(1)
+        self.assertEqual(res, 4)
+
+    def test_perimeter_neg_add(self):
+        res = perimeter(-3)
+        self.assertFalse(res)
